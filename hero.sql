0685ee70b932d7fce2ae789a294e6e1b4e467ffa
kimirut <kimrigodon7@gmail.com>
2023-11-08 00:51:51 +0800

    feat select average player level per class

--- hero.sql
+++ hero.sql
@@ -176,3 +176,11 @@ FROM public.hero h
 JOIN public.class c ON h.class_id = c.class_id
 WHERE c.class_name = 'Archer';
 
+--7
+--Retrieve the average player level for each class, arranging them in descending order from the highest level to the lowest.
+SELECT c.class_name, AVG(p.player_level) AS average_level
+FROM public.class c
+LEFT JOIN public.hero h ON c.class_id = h.class_id
+LEFT JOIN public.player p ON h.hero_id = p.hero_id
+GROUP BY c.class_name
+ORDER BY average_level DESC;
